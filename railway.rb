d325662320a7c951e193639ba53a9ca66f22f046
Jake Runzer <jakerunzer@gmail.com>
2020-10-01 17:44:09 -0600

    update install url

--- railway.rb
+++ railway.rb
@@ -3,7 +3,7 @@
 class Railway < Formula
   desc "Develop and deploy code with zero configuration"
   homepage "https://railway.app"
-  url "https://github.com/railwayapp/cli/releases/download/v0.0.1/railway-1.0.0.tar.gz"
+  url "https://github.com/railwayapp/cli/releases/download/v0.0.1/railway"
   sha256 "f73d45833e332bfe248c926e0668898586f3ceabebbd870400d41f144f547067"
   version "0.0.1"
 
