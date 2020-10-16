977f2dae024b641830179a51557da92f175556a8
goreleaserbot <goreleaser@railway.app>
2020-10-16 15:32:44 -0600

    Brew formula update for railway version v0.1.5

--- railway.rb
+++ railway.rb
@@ -2,16 +2,16 @@
 class Railway < Formula
   desc "Develop and deploy code with zero configuration"
   homepage "https://railway.app"
-  version "0.1.4"
+  version "0.1.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/railwayapp/cli/releases/download/v0.1.4/railway_0.1.4_darwin_amd64.tar.gz"
-    sha256 "07149757281dd85cbf81d9328004ca504d7215b5cadb5a1f97ddd744b5dde297"
+    url "https://github.com/railwayapp/cli/releases/download/v0.1.5/railway_0.1.5_darwin_amd64.tar.gz"
+    sha256 "7c2f95150b041fe41ed050e1cb70654a015b361c4a638d21099dadb47a49ddaa"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/railwayapp/cli/releases/download/v0.1.4/railway_0.1.4_linux_amd64.tar.gz"
-      sha256 "8a8461168ca29d1ab94d3a717738cd5b7d9ce426fa8dd40b4a11084362d61e32"
+      url "https://github.com/railwayapp/cli/releases/download/v0.1.5/railway_0.1.5_linux_amd64.tar.gz"
+      sha256 "c45bf7da4f80dc2a6f7512be36bf40691b1fd45e8637d45fc518ba9aa949a3b1"
     end
   end
 
