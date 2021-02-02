4b3e272bc54448f5eb351e240be1326add1a8338
goreleaserbot <goreleaser@railway.app>
2021-02-02 11:03:36 -0700

    Brew formula update for railway version v0.1.26

--- railway.rb
+++ railway.rb
@@ -5,20 +5,20 @@
 class Railway < Formula
   desc "Develop and deploy code with zero configuration"
   homepage "https://railway.app"
-  version "0.1.25"
+  version "0.1.26"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/railwayapp/cli/releases/download/v0.1.25/railway_0.1.25_darwin_amd64.tar.gz"
-    sha256 "62458522ab6e4d013442fa4bfac3c2f59f92ac7e25a0b526b86bbd5d074a0406"
+    url "https://github.com/railwayapp/cli/releases/download/v0.1.26/railway_0.1.26_darwin_amd64.tar.gz"
+    sha256 "392941496ff94c685989f74efd1f78e9d0a364423196e1d5c7c043342ce0e399"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/railwayapp/cli/releases/download/v0.1.25/railway_0.1.25_linux_amd64.tar.gz"
-    sha256 "2e52ba93f10ea908438c7e73791ac6e9ff24e45983d844934799c2bc60a1584b"
+    url "https://github.com/railwayapp/cli/releases/download/v0.1.26/railway_0.1.26_linux_amd64.tar.gz"
+    sha256 "f8441da5cc45b12e0f33c4f85f79a28db83a8daa2e6189157b1671f87f671d37"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/railwayapp/cli/releases/download/v0.1.25/railway_0.1.25_linux_arm64.tar.gz"
-    sha256 "6966e45565dd4abe9c8e5d6c96b60112dd0339866b43895344669834dc116195"
+    url "https://github.com/railwayapp/cli/releases/download/v0.1.26/railway_0.1.26_linux_arm64.tar.gz"
+    sha256 "a03dbcc068f9f8ffc4337a9a9bed7c8419ad25a9ed3bd49f9378c9f3c548d8f8"
   end
 
   def install
