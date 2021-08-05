c165f7be4b981f6e84d58bf19b371cc9798f6e88
goreleaserbot <goreleaser@railway.app>
2021-08-04 19:25:27 -0700

    Brew formula update for railway version v0.2.43

--- railway.rb
+++ railway.rb
@@ -5,28 +5,28 @@
 class Railway < Formula
   desc "Develop and deploy code with zero configuration"
   homepage "https://railway.app"
-  version "0.2.42"
+  version "0.2.43"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/railwayapp/cli/releases/download/v0.2.42/railway_0.2.42_darwin_amd64.tar.gz"
-      sha256 "39e7e7dcdbffb9b0df4b0e063f18247b5af592cbc951754d78a37ebb267fb71c"
+      url "https://github.com/railwayapp/cli/releases/download/v0.2.43/railway_0.2.43_darwin_amd64.tar.gz"
+      sha256 "c890bba6c855c9fea2ebded55396d732b9c109145cfb0524b010270f77cd4201"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/railwayapp/cli/releases/download/v0.2.42/railway_0.2.42_darwin_arm64.tar.gz"
-      sha256 "d685318e3b37979bcff4accf57f1c664fcf83512d2c08107ceb8671b4fae1a48"
+      url "https://github.com/railwayapp/cli/releases/download/v0.2.43/railway_0.2.43_darwin_arm64.tar.gz"
+      sha256 "b3cc5b0d023808cd5bdfadf90fa720d2b59512e1feba9f9b073e1f745893136c"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/railwayapp/cli/releases/download/v0.2.42/railway_0.2.42_linux_amd64.tar.gz"
-      sha256 "ce9cee372185d266467e8f7a6ed6473d8b128fd6aac25789583c90ffda59b34d"
+      url "https://github.com/railwayapp/cli/releases/download/v0.2.43/railway_0.2.43_linux_amd64.tar.gz"
+      sha256 "48869fefb90d5e39a8b7d30869cee658ce5cb5ebb0ecd0ad20963937593c6bed"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/railwayapp/cli/releases/download/v0.2.42/railway_0.2.42_linux_arm64.tar.gz"
-      sha256 "1bd131b171fd650d07a1ba79f397f5e340a03eeb1cb916a4fad0508712c4ffee"
+      url "https://github.com/railwayapp/cli/releases/download/v0.2.43/railway_0.2.43_linux_arm64.tar.gz"
+      sha256 "1f576b57d86aaaf3f6cc8989b06e22edc9b451a0c20478d2d227f1cc7a9f8105"
     end
   end
 
