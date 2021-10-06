a536a34981a05fb8526fe0c2bbbefb55137948dd
goreleaserbot <goreleaser@railway.app>
2021-10-06 12:08:18 -0700

    Brew formula update for railway version v0.2.44

--- railway.rb
+++ railway.rb
@@ -5,28 +5,28 @@
 class Railway < Formula
   desc "Develop and deploy code with zero configuration"
   homepage "https://railway.app"
-  version "0.2.43"
+  version "0.2.44"
   bottle :unneeded
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/railwayapp/cli/releases/download/v0.2.43/railway_0.2.43_darwin_amd64.tar.gz"
-      sha256 "c890bba6c855c9fea2ebded55396d732b9c109145cfb0524b010270f77cd4201"
-    end
     if Hardware::CPU.arm?
-      url "https://github.com/railwayapp/cli/releases/download/v0.2.43/railway_0.2.43_darwin_arm64.tar.gz"
-      sha256 "b3cc5b0d023808cd5bdfadf90fa720d2b59512e1feba9f9b073e1f745893136c"
+      url "https://github.com/railwayapp/cli/releases/download/v0.2.44/railway_0.2.44_darwin_arm64.tar.gz"
+      sha256 "45ed34b12e4321567c974dfb1b9f65c11819fd0e4cdddc3bb2401ddca3a6d6bd"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/railwayapp/cli/releases/download/v0.2.44/railway_0.2.44_darwin_amd64.tar.gz"
+      sha256 "24b5ab70a4f84e3a7c9d75903a53f7bf6245a72eb7aa4bd97a938463ceec4449"
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/railwayapp/cli/releases/download/v0.2.43/railway_0.2.43_linux_amd64.tar.gz"
-      sha256 "48869fefb90d5e39a8b7d30869cee658ce5cb5ebb0ecd0ad20963937593c6bed"
-    end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/railwayapp/cli/releases/download/v0.2.43/railway_0.2.43_linux_arm64.tar.gz"
-      sha256 "1f576b57d86aaaf3f6cc8989b06e22edc9b451a0c20478d2d227f1cc7a9f8105"
+      url "https://github.com/railwayapp/cli/releases/download/v0.2.44/railway_0.2.44_linux_arm64.tar.gz"
+      sha256 "04863ad14bfdaef2074de7544612bcd6ddb64fd59e1f8b43a712ff6ee639b836"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/railwayapp/cli/releases/download/v0.2.44/railway_0.2.44_linux_amd64.tar.gz"
+      sha256 "ddcc6bcb6ea3dc0949a3138e5ac37a544a327067334ddb0ee9207251d1a4fb23"
     end
   end
 
